ff537e809edd69726d0d760d9029434066b28ef4
krucialbe <sedrickmoore2@gmail.com>
2025-01-23 13:49:09 -0800

    changed read me

--- app.config.js
+++ app.config.js
@@ -1,9 +1,17 @@
 export default {
   expo: {
-    // other configurations...
     extra: {
       EXPO_PUBLIC_SUPABASE_URL: process.env.EXPO_PUBLIC_SUPABASE_URL,
       EXPO_PUBLIC_SUPABASE_ANON_KEY: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+      eas: {
+        projectId: "ae1c99d9-c10c-4657-9882-40e5435468fd",
+      },
+    },
+    android: {
+      package: "com.sedrickmoore.snaptogether", // Replace with your unique Android package name
+    },
+    ios: {
+      bundleIdentifier: "com.sedrickmoore.snaptogether", // Replace with your unique iOS bundle identifier
     },
     plugins: [
       [
